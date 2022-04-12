55be24a433bdb0dd8b16c1339b4f212c535dc1d6
kazem_str173111 <kazem.sattari@gmail.com>
2022-04-12 14:11:23 -0700

    =add all files in the project

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+</head>
+<body>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa tenetur minima ipsum doloribus porro reprehenderit, excepturi atque esse pariatur recusandae, quisquam quasi expedita quaerat eum sed. Incidunt nemo alias neque.
+        Commodi laboriosam eveniet animi officiis facere, deleniti iure, voluptatem delectus, modi ut similique vero quidem rem nihil minima. Aliquam distinctio illo unde reprehenderit sed rem labore cumque, possimus eveniet excepturi.
+        Cumque sint debitis tenetur, est ipsa totam nisi qui aliquam adipisci consequatur! Cumque placeat maiores impedit ab inventore harum explicabo quidem aut, sint rem labore molestiae nesciunt laudantium aliquam quo.</p>
+        
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>index</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus officiis reprehenderit molestias ab rem minima eaque sequi, totam saepe, ex quia vitae necessitatibus praesentium quo asperiores. Reprehenderit commodi cumque at.
+    Atque iste reprehenderit eius obcaecati magnam, numquam aliquam omnis quaerat dolore accusamus optio, error fugiat cumque iusto blanditiis cum. Minus vel ipsum quod incidunt eaque ut veritatis enim, saepe unde.
+    Accusantium, accusamus dicta soluta debitis ab tenetur autem enim ea quisquam dolorem explicabo, quia laboriosam ipsa eveniet officiis amet, necessitatibus esse sequi! Saepe eaque corrupti dolore quaerat quasi perspiciatis nisi!
+    Fugit vel illo impedit unde nulla fugiat eveniet magni officiis aliquid distinctio corrupti modi numquam optio esse, doloribus eius perspiciatis quisquam dolores incidunt dolor ipsum quidem sint dicta. Veritatis, perferendis.
+    Facilis, ex molestiae cum eligendi delectus suscipit assumenda impedit perferendis saepe distinctio, consequuntur ducimus recusandae laudantium quas odit nobis eius in autem voluptatem soluta consectetur! Nam vel saepe nesciunt aut.</p>
+
+</body>
+</html>
