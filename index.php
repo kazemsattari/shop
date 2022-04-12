cd6fb999b58fa58c32b86d255f1ea6a7eedeceeb
kazem_str173111 <kazem.sattari@gmail.com>
2022-04-12 14:28:24 -0700

    =change index.php

--- index.php
+++ index.php
@@ -8,14 +8,10 @@
     <title>index</title>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus officiis reprehenderit molestias ab rem minima eaque sequi, totam saepe, ex quia vitae necessitatibus praesentium quo asperiores. Reprehenderit commodi cumque at.
-    Atque iste reprehenderit eius obcaecati magnam, numquam aliquam omnis quaerat dolore accusamus optio, error fugiat cumque iusto blanditiis cum. Minus vel ipsum quod incidunt eaque ut veritatis enim, saepe unde.
-    Accusantium, accusamus dicta soluta debitis ab tenetur autem enim ea quisquam dolorem explicabo, quia laboriosam ipsa eveniet officiis amet, necessitatibus esse sequi! Saepe eaque corrupti dolore quaerat quasi perspiciatis nisi!
-    Fugit vel illo impedit unde nulla fugiat eveniet magni officiis aliquid distinctio corrupti modi numquam optio esse, doloribus eius perspiciatis quisquam dolores incidunt dolor ipsum quidem sint dicta. Veritatis, perferendis.
-    Facilis, ex molestiae cum eligendi delectus suscipit assumenda impedit perferendis saepe distinctio, consequuntur ducimus recusandae laudantium quas odit nobis eius in autem voluptatem soluta consectetur! Nam vel saepe nesciunt aut.</p>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae voluptate, tenetur nobis sed numquam incidunt dignissimos excepturi beatae quasi labore fugiat illo a aliquid mollitia. Eum laborum officiis qui inventore.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, sapiente. Sunt maxime magnam quae omnis odio non officiis exercitationem repellat nisi doloremque porro excepturi, nemo eveniet fugiat hic reiciendis tempore?</p>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit ratione distinctio ipsa id inventore, ipsam ex sequi, similique, provident laudantium numquam. Dignissimos a quasi officia, distinctio quae iste repudiandae vitae?</p>
-
+    <section>
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum a repudiandae iure, nihil unde laboriosam quod ex, aut, possimus sapiente iusto? Corporis hic quas magni vero pariatur placeat excepturi quo!</p>
+        <a href="#">read-more</a>
+    </section>
 </body>
 </html>
